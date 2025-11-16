1ffd3453450c99586e30f3d4b937db271d9b2c09
Schmidtfe <116957574+Schmidtfe@users.noreply.github.com>
2025-11-16 12:02:33 +0100

    Fixed Player Physics and added new sprites

--- Player.cs
+++ Player.cs
@@ -2,6 +2,8 @@ using Godot;
 using System;
 using System.Diagnostics;
 using System.Net;
+using System.Numerics;
+using Vector2 = Godot.Vector2;
 
 public partial class Player : CharacterBody2D
 {
@@ -10,32 +12,28 @@ public partial class Player : CharacterBody2D
     [Export] public float maxSpeed = 3000;
     [Export] public float jumpForce = 3000;
     [Export] public float friction = 0.05f;
+    [Export] public float bounciness = 1.0f;
     [Export] public Control arrow;
     private Vector2 direction = new Vector2(1, 0);
     private Vector2 forwardDir = new Vector2(1, 0);
     private bool pressingRight = false;
     private bool pressingLeft = false;
     private bool pressingJump = false;
-    private bool bounced = false;
     private Vector2 lastVelocity = Vector2.Zero;
     private float prevRot = 0;
     private AnimationPlayer animationPlayer;
-    private bool isOnSlope = false;
     private Sprite2D sprite;
+    private String velocityInfo;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print("Player is ready");
-        /*Vector2 vecA = new Vector2(0, 10);
-        Vector2 vecB = new Vector2(0, -1);
-        Vector2 projNV = vecA.Normalized() * vecB / Mathf.Pow(vecB.Length(), 2) * vecB;
-        Vector2 c = vecA.Normalized() - projNV;
-        GD.Print(vecA.Slide(projNV + c));*/
-        GD.Print("Scale: " + Scale);
         sprite = GetNode<Sprite2D>("Sprite");
         animationPlayer = GetNode<AnimationPlayer>("Sprite/AnimationPlayer");
         animationPlayer.Play("Idle");
+        GD.Print(Mathf.Max(-2000, -5000));
+        
+        GD.Print("Player is ready");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -45,39 +43,37 @@ public partial class Player : CharacterBody2D
 
     public override void _PhysicsProcess(double delta)
     {
+        velocityInfo = "";
         float floorAngle = 0;
-        bool bounced = false;
         if (lastVelocity != Vector2.Zero)
         {
-            isOnSlope = false;
+            String collisions = "";
             for (int i = 0; i < GetSlideCollisionCount(); i++)
             {
-                //GD.Print("Colliding with " + ((Node)GetSlideCollision(i).GetCollider()).GetParent<ColorRect>().Name);
                 Vector2 collisionNormal = GetSlideCollision(i).GetNormal();
-                //GD.Print("Rotation of " + lastVelocity + " and " + collisionNormal + " = " + RotationOfVectors(lastVelocity, collisionNormal));
                 float collisionAngle = RotationOfVectors(direction, collisionNormal);
-                if (collisionAngle > 160f)
+                if (collisionAngle < 15f)
                 {
-                    //Velocity = lastVelocity.Bounce(collisionNormal);
-                    //GD.Print("Bounce Velocity: " + Velocity);
-                    bounced = true;
-                    direction.X *= -1;
+                    Velocity = lastVelocity.Bounce(collisionNormal) * bounciness;
+                    velocityInfo += "Post Bounce Calc (" + ((Node)GetSlideCollision(i).GetCollider()).GetParent().Name + "): " + Velocity +"\n";
                 }
                 else
                 {
                     Velocity = lastVelocity.Slide(collisionNormal);
+                    velocityInfo += "Post Slide Calc (" + ((Node)GetSlideCollision(i).GetCollider()).GetParent().Name + "): " + Velocity + " " + collisionNormal +"\n";
                 }
-
-                if (collisionAngle is < 150 and > 120)
+                
+                float floorAngle2 = GetSlideCollision(i).GetAngle() * 180 / Mathf.Pi;
+                collisions += ((Node)GetSlideCollision(i).GetCollider()).GetParent().Name + ": " + floorAngle2 + " // " + collisionNormal + "\n";
+                if(floorAngle2 < 15)
                 {
-                    isOnSlope = true;
-                    floorAngle = RotationOfVectors(collisionNormal, Vector2.Up);
+                    Velocity *= Vector2.Right;
                 }
             }
         }
         
-        if(!IsOnFloor())
-            Velocity += new Vector2(0, 983.4f * mass * (float)delta);
+        Velocity += new Vector2(0, 983.4f * mass * (float)delta);
+        velocityInfo += "Post Gravity: " + Velocity + "\n";
 
         if (Velocity.X is < 0.5f and > -0.5f)
             Velocity = new Vector2(0, Velocity.Y);
@@ -86,10 +82,12 @@ public partial class Player : CharacterBody2D
 
         //Input
         if (pressingJump && IsOnFloor())
+        {
+            Velocity *= Vector2.Right;
             Velocity += new Vector2(0, -jumpForce * 10 * (float)delta);
+        }
 
-        /*if (!pressingJump && !IsOnFloor() && Velocity.Y < 0)
-            Velocity = new Vector2(Velocity.X, 0);*/
+        velocityInfo += "Post Jump: " + Velocity + "\n";
         
         Vector2 addedForce = Vector2.Zero;
         
@@ -97,39 +95,29 @@ public partial class Player : CharacterBody2D
         {
             forwardForce = speed * 10 * (float)delta * Mathf.Min(maxSpeed - Velocity.X, maxSpeed) / maxSpeed;
             addedForce = forwardForce * Vector2.Right;
-            addedForce.Rotated(floorAngle);
-            //GD.Print(addedForce);
         }
         
         if (pressingLeft)
         {
-            forwardForce = speed * 10 * (float)delta * Mathf.Abs(Mathf.Max(maxSpeed - Velocity.X, -maxSpeed)) / maxSpeed;
+            forwardForce = speed * 10 * (float)delta * Mathf.Abs(Mathf.Max(-maxSpeed - Velocity.X, -maxSpeed)) / maxSpeed;
             addedForce = forwardForce * Vector2.Left;
-            addedForce.Rotated(floorAngle);
-            //GD.Print(addedForce);
         }
         
         Velocity += addedForce;
+        velocityInfo += "Post Force Add: " + Velocity + "\n";
 
-        if (!pressingRight && IsOnFloor())
+        if (!(pressingRight &&  Velocity.X > 0 || pressingLeft &&  Velocity.X < 0) && IsOnFloor())
             Velocity -= new Vector2(friction * Velocity.X, 0);
+        
+        velocityInfo += "Post Jump: " + Velocity + "\n";
+        
+        //GD.Print(velocityInfo);
         
         UpdateSprite();
         
         lastVelocity = Velocity;
 
-
         MoveAndSlide();
-    }
-
-    private Vector2 GetSlopePotency(Vector2 angleDirection)
-    {
-        float rot = RotationOfVectors(angleDirection, direction);
-        float potency = 1;
-        if(rot is < 90 and > 0)
-            potency = rot / 90f;
-        GD.Print(angleDirection);
-        return angleDirection * potency;
     }
 
     private bool AboutSameVector(Vector2 vec1, Vector2 vec2, float errorAngle)
@@ -182,24 +170,35 @@ public partial class Player : CharacterBody2D
     {
         //GD.Print("Press Right: " + pressingRight + " // Press Left: " + pressingLeft);
         if(Velocity.X > 0 && sprite.Scale.X < 0)
-            sprite.Scale = new Vector2(1, 1);
+            sprite.Scale = new Vector2(Mathf.Abs(sprite.Scale.X), sprite.Scale.Y);
         else if(Velocity.X < 0 && sprite.Scale.X > 0)
-            sprite.Scale = new Vector2(-1, 1);
+            sprite.Scale = new Vector2(-Mathf.Abs(sprite.Scale.X), sprite.Scale.Y);
         
         if (Mathf.Abs(Velocity.X) > 0)
         {
             if(Mathf.Abs(lastVelocity.X) <= 0)
                 animationPlayer.Play("Roll");
 
-            if (Mathf.Abs(Velocity.X) >= 200 && Mathf.Abs(lastVelocity.X) < 200)
-                GetNode<Sprite2D>("Sprite").Frame = 4;
-            
-            if (Mathf.Abs(Velocity.X) < 200 && Mathf.Abs(lastVelocity.X) >= 200)
-                GetNode<Sprite2D>("Sprite").Frame = 0;
-            
+            switch (Mathf.Abs(Velocity.X))
+            {
+                case float n when (n < 800):
+                    GetNode<Sprite2D>("Sprite").Frame = 0;
+                    break;
+                
+                case float n when (n >= 800 && n < 1600):
+                    GetNode<Sprite2D>("Sprite").Frame = 3;
+                    break;
+                
+                case float n when (n >= 1600 && n < 3200):
+                    GetNode<Sprite2D>("Sprite").Frame = 4;
+                    break;
+                
+                case float n when (n >= 3200):
+                    GetNode<Sprite2D>("Sprite").Frame = 5;
+                    break;
+            }
+
             animationPlayer.SpeedScale = Velocity.X / maxSpeed * 5;
-            
-            GD.Print(animationPlayer.SpeedScale);
         }
         else if(Mathf.Abs(lastVelocity.X) > 0)
         {
